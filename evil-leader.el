1a5d59f122729118bc24d07a7f3c1b61c34b6b19
Michael Markert <markert.michael@googlemail.com>
2013-03-16 14:14:03 +0100

    Update to 0.2.1.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- evil-leader.el
+++ evil-leader.el
@@ -5,7 +5,7 @@
 ;; URL: http://github.com/cofi/evil-leader
 ;; Git-Repository: git://github.com/cofi/evil-leader.git
 ;; Created: 2011-09-13
-;; Version: 0.2
+;; Version: 0.2.1
 ;; Keywords: evil vim-emulation leader
 ;; Package-Requires: ((evil "0"))
 

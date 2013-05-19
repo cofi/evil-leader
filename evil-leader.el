88ce08dd2a402f76e5938b69b7e6540fdd573e9d
Michael Markert <markert.michael@googlemail.com>
2013-05-20 00:05:01 +0200

    Update docs.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- evil-leader.el
+++ evil-leader.el
@@ -33,10 +33,41 @@
 ;; (require 'evil-leader)
 
 ;; Usage:
-
-;; bind keys using `evil-leader/set-key'
-;; and call them via <leader>key or <prefixed-leader>key in emacs/insert state
-;; if `evil-leader/in-all-states' is non-nil
+;;
+;; (global-evil-leader-mode)
+;;
+;;    to enable `evil-leader' in every buffer where `evil' is enabled.
+;;
+;;    Use `evil-leader/set-key' to bind keys in the leader map.
+;;    For example:
+;;
+;; (evil-leader/set-key "e" 'file-file)
+;;
+;;    You can also bind several keys at once:
+;;
+;; (evil-leader/set-key
+;;   "e" 'file-file
+;;   "b" 'switch-to-buffer
+;;   "k" 'kill-buffer)
+;;
+;;    The key map can of course be filled in several places.
+;;
+;;    After you set up the key map you can access the bindings by pressing =<leader>=
+;;    (default: \) and the key(s). E.g. \ e would call `find-file' to open a file.
+;;
+;;    If you wish to change so you can customize =evil-leader/leader= or call
+;;    `evil-leader/set-leader', e.g. (evil-leader/set-leader ",") to change it to
+;;    ",".
+;;    The leader has to be readable by `read-kbd-macro', so using Space as a
+;;    prefix key would be (evil-leader/set-leader "<Space>").
+;;
+;;    Beginning with version 0.3 evil-leader has support for mode-local bindings:
+;;
+;; (evil-leader/set-key-for-mode 'emacs-lisp-mode "b" 'byte-compile-file)
+;;
+;;    Again, you can bind several keys at once.
+;;
+;;    A mode-local binding shadows a normal mode-independent binding.
 
 ;;; Code:
 

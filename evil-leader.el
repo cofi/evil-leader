bfd92a7c53d556bac694615412997d22f886eadc
Michael Markert <markert.michael@googlemail.com>
2014-03-07 22:04:08 +0100

    Bump to 0.4.2

--- evil-leader.el
+++ evil-leader.el
@@ -5,7 +5,7 @@
 ;; URL: http://github.com/cofi/evil-leader
 ;; Git-Repository: git://github.com/cofi/evil-leader.git
 ;; Created: 2011-09-13
-;; Version: 0.4.1
+;; Version: 0.4.2
 ;; Keywords: evil vim-emulation leader
 ;; Package-Requires: ((evil "0"))
 
